2c811e143db9986cb025afb56eb626f7006dc0b7
d.tovstolug <d.tovstolug@simcord.com>
2022-06-21 19:55:53 +0300

    add images

--- autostore.sql
+++ autostore.sql
@@ -75,12 +75,12 @@ INSERT INTO client(login, password) VALUES
 ("test", "123123");
 
 INSERT INTO bike(typeid, firmid, model, frame, seat, brakes, "chain", image) VALUES
-(1,1, 'G7 Pro', 'Global Concept G7 Pro Carbon Monocoque', 'Prologo Kappa RS', 'Shimano Ultegra DM', 'FSA TH-CN1102', ''),
-(1,2, 'Optimo 4', 'SmartForm C2 Alloy', 'Cannondale Stage CX', 'Promax RC-452', 'Shimano HG71', ''),
-(2,3, 'Specialissima CV Ultegra Di2 11s Stages LR', 'BIANCHI Specialissima 57cm Celeste', 'Shimano Ultegra DI2 R8050 11 s', 'BIANCHI Fizik Arione R3 304x126mm Black', 'SHIMANO CN-HG701', ''),
-(3,4 , 'Dolphin 1.0', 'Shimano', 'Trinx Comfort', 'Tektro', 'M30 7S', ''),
-(3,5 , 'E300 7', 'E300 frame', 'Hollow Thickened Wide-tailed', 'SHIMANO', 'Chengshin', ''),
-(4,6 , 'Race FE 28', 'Aluminium 6061 T6 Superlite', 'Natural Fit Venec Lite', ' Shimano BR-RX400', 'Shimano CN-HG600-11', '');
+(1,1, 'G7 Pro', 'Global Concept G7 Pro Carbon Monocoque', 'Prologo Kappa RS', 'Shimano Ultegra DM', 'FSA TH-CN1102', 'https://images.prom.ua/3770322257_velosiped-shossejnyj-bh.jpg'),
+(1,2, 'Optimo 4', 'SmartForm C2 Alloy', 'Cannondale Stage CX', 'Promax RC-452', 'Shimano HG71', 'https://veloplaneta.ua/resize_1438x809x80/upload/iblock/2ea/91576181-53cc-11ea-b5e4-9f33bfe521cd_b99d30e4-2e37-11eb-981a-834758a193ec.webp'),
+(2,3, 'Specialissima CV Ultegra Di2 11s Stages LR', 'BIANCHI Specialissima 57cm Celeste', 'Shimano Ultegra DI2 R8050 11 s', 'BIANCHI Fizik Arione R3 304x126mm Black', 'SHIMANO CN-HG701', 'https://www.veloonline.com/images/catalog/list/big/big_merida-mission-cx-5000-silk-silver-red-_20537_pic.jpg'),
+(3,4 , 'Dolphin 1.0', 'Shimano', 'Trinx Comfort', 'Tektro', 'M30 7S', 'https://i.allo.ua/media/catalog/product/cache/1/image/710x600/602f0fa2c1f0d1ba5e241f914e856ff9/c/8/c84c0485f7e145d958244695d4f2092d.jpg'),
+(3,5 , 'E300 7', 'E300 frame', 'Hollow Thickened Wide-tailed', 'SHIMANO', 'Chengshin', 'https://www.dhresource.com/webp/m/0x0/f2/albu/g9/M00/71/0D/rBVaWF4Rpq2AIdY9AAFQa9BObzA055.jpg'),
+(4,6 , 'Race FE 28', 'Aluminium 6061 T6 Superlite', 'Natural Fit Venec Lite', ' Shimano BR-RX400', 'Shimano CN-HG600-11', 'https://www.veloonline.com/images/catalog/list/big/big_velosiped-cube-nuroad-race-fe-28-blackir_37568.jpg');
 
 
 INSERT INTO dillercenter(name, address) VALUES
